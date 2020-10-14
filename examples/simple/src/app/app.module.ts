806881d6df65f459937782e1277790813b672aba
Marc-Antoine Ferland <maferland@hey.com>
2020-10-13 22:52:02 -0400

    feat: simplify simple example

--- examples/simple/src/app/app.module.ts
+++ examples/simple/src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
